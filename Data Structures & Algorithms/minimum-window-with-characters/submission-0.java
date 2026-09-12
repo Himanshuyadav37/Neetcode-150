42d4b1c85347bc6389a1d5ab4e2cacb0bb991c9c
Himanshu <167640226+Himanshuyadav37@users.noreply.github.com>
2026-09-12 14:03:25 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.java
@@ -0,0 +1,55 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (t.length() > s.length()) return "";
+
+        HashMap<Character, Integer> need = new HashMap<>();
+        HashMap<Character, Integer> window = new HashMap<>();
+
+        // Required characters
+        for (char c : t.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0;
+        int have = 0;
+        int required = need.size();
+
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            char c = s.charAt(right);
+            window.put(c, window.getOrDefault(c, 0) + 1);
+
+            // Requirement for this character is completed
+            if (need.containsKey(c) &&
+                window.get(c).intValue() == need.get(c)) {
+                have++;
+            }
+
+            // Try to make window smaller
+            while (have == required) {
+
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char remove = s.charAt(left);
+                window.put(remove, window.get(remove) - 1);
+
+                if (need.containsKey(remove) &&
+                    window.get(remove) < need.get(remove)) {
+                    have--;
+                }
+
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE
+                ? ""
+                : s.substring(start, start + minLen);
+    }
+}
